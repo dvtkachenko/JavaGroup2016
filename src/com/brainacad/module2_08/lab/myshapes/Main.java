a7dec58b14593f298ce5b4ceaf2232d3f33571d6
diman1977 <smartphone000014@gmail.com>
2017-01-30 21:18:51 +0200

    lab2_8

--- src/com/brainacad/module2_08/lab/myshapes/Main.java
+++ src/com/brainacad/module2_08/lab/myshapes/Main.java
@@ -1,8 +1,21 @@
 package com.brainacad.module2_08.lab.myshapes;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 // lab2_8_2
 interface Drawable {
 	void draw();
+}
+
+class ShapeColorComparator implements Comparator {
+	@Override
+	public int compare(Object o1, Object o2) {
+
+		Shape s1 = (Shape) o1;
+		Shape s2 = (Shape) o2;
+		return s1.getShapeColor().compareTo(s2.getShapeColor());
+	}
 }
 
 // lab2_8_1
@@ -28,7 +41,7 @@ abstract class Shape implements Drawable {
 
 }
 
-class Circle extends Shape {
+class Circle extends Shape implements Comparable {
 	private double radius;
 	
 	Circle(String color, double radius) {
@@ -42,6 +55,14 @@ class Circle extends Shape {
 		System.out.printf("%.2f", calcArea());
 		System.out.println();
 		System.out.println();
+	}
+
+	// implements interface Comparable
+	public int compareTo(Object o) {
+		Circle circle = (Circle) o;
+		if (this.calcArea() > circle.calcArea()) return 1;
+		if (this.calcArea() < circle.calcArea()) return -1;
+		return 0;
 	}
 
 	public double getRadius() {
@@ -64,7 +85,7 @@ class Circle extends Shape {
 	
 }
 
-class Rectangle extends Shape {
+class Rectangle extends Shape implements Comparable {
 	private double width, height;
 	
 	Rectangle(String color, double width, double height) {
@@ -79,6 +100,18 @@ class Rectangle extends Shape {
 		System.out.printf("%.2f", calcArea());
 		System.out.println();
 		System.out.println();
+	}
+
+	// implements interface Comparable
+	public int compareTo(Object o) {
+		Rectangle rectangle = (Rectangle) o;
+		if (this.calcArea() > rectangle.calcArea()) return 1;
+		if (this.calcArea() < rectangle.calcArea()) return -1;
+		return 0;
+		// ternary comparation
+//		 return this.calcArea() > rectangle.calcArea() ? 1 :
+//				this.calcArea() < rectangle.calcArea() ? -1 : 0;
+
 	}
 
 	public double getWidth() {
@@ -110,7 +143,7 @@ class Rectangle extends Shape {
 	
 }
 
-class Triangle extends Shape {
+class Triangle extends Shape implements Comparable {
 	private double a, b, c;
 	
 	Triangle(String color, double a, double b, double c) {
@@ -126,6 +159,13 @@ class Triangle extends Shape {
 		System.out.printf("%.2f", calcArea());
 		System.out.println();
 		System.out.println();
+	}
+	// implements interface Comparable
+	public int compareTo(Object o) {
+		Triangle triangle = (Triangle) o;
+		if (this.calcArea() > triangle.calcArea()) return 1;
+		if (this.calcArea() < triangle.calcArea()) return -1;
+		return 0;
 	}
 
 	public double getA() {
@@ -169,6 +209,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// Lab 2_8_2
+		System.out.println("LAB 2_8_2");
+		System.out.println();
+
 		Shape[] arrOfShapes = new Shape[9];
 		
 		arrOfShapes[0] = new Rectangle("Red", 3, 8);
@@ -182,6 +225,57 @@ public class Main {
 		arrOfShapes[8] = new Triangle("Magenta", 6, 22, 18);
 
 		for(Shape sh : arrOfShapes) {
+			sh.draw();
+		}
+
+		// lab2_8_3
+		System.out.println("LAB 2_8_3");
+		System.out.println();
+
+		Rectangle r1 = new Rectangle("Red", 3, 8);
+		Rectangle r2 = new Rectangle("Green", 5, 12);
+
+		System.out.println("Compare to instances of Rectangle");
+		System.out.println(r1.compareTo(r2));
+		System.out.println(r2.compareTo(r1));
+		System.out.println();
+
+		// lab2_8_4
+		System.out.println("LAB 2_8_4");
+		System.out.println();
+
+		Rectangle[] arrOfRectangle = new Rectangle[6];
+		arrOfRectangle[0] =  new Rectangle("Red", 23, 45);
+		arrOfRectangle[1] =  new Rectangle("Green", 1, 4);
+		arrOfRectangle[2] =  new Rectangle("Black", 34, 79);
+		arrOfRectangle[3] =  new Rectangle("Grey", 32, 48);
+		arrOfRectangle[4] =  new Rectangle("Magenta", 20, 80);
+		arrOfRectangle[5] =  new Rectangle("Yellow", 17, 97);
+
+		Arrays.sort(arrOfRectangle);
+		for(Rectangle r : arrOfRectangle) {
+			r.draw();
+		}
+
+		// lab2_8_5
+		System.out.println("LAB 2_8_5");
+		System.out.println();
+
+		Shape[] arr = new Shape[10];
+
+		arr[0] = new Rectangle("Purple", 3, 8);
+		arr[1] = new Rectangle("Brown", 45, 9);
+		arr[2] = new Rectangle("Black", 4, 12);
+		arr[3] = new Rectangle("Blue", 3, 15);
+		arr[4] = new Rectangle("Cayan", 21, 10);
+		arr[5] = new Circle("Yellow", 12);
+		arr[6] = new Circle("Red", 15);
+		arr[7] = new Triangle("Orange", 7, 14, 20);
+		arr[8] = new Triangle("Pink", 6, 22, 18);
+		arr[9] = new Triangle("White", 6, 22, 18);
+
+		Arrays.sort(arr, new ShapeColorComparator());
+		for(Shape sh : arr) {
 			sh.draw();
 		}
 
