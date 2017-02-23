65968e97093d8b1b8858bbe678480a40bef65e77
diman1977 <smartphone000014@gmail.com>
2017-02-23 16:20:18 +0200

    lab2_17

---
+++ src/com/brainacad/module2_17/lab/test_thread1/MyTimeBomb.java
@@ -0,0 +1,60 @@
+package com.brainacad.module2_17.lab.test_thread1;
+
+/**
+ * Created by Дима on 23.02.2017.
+ */
+
+// lab2_17_1
+class MyTimeBombThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Number : " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Boom !!!");
+    }
+}
+
+// lab2_17_2
+class MyTimeBombRunnable implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Number : " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Boom !!!");
+    }
+}
+
+// lab2_17_1
+class MainThread {
+    public static void main(String[] args) {
+        System.out.println("lab2_17_1 - method main start");
+        MyTimeBombThread bombThread = new MyTimeBombThread();
+        bombThread.start();
+        System.out.println("lab2_17_1 - method main finish");
+
+    }
+}
+
+// lab2_17_2
+class MainRunnable {
+    public static void main(String[] args) {
+        System.out.println("lab2_17_2 - method main start");
+        MyTimeBombRunnable bombRunnable = new MyTimeBombRunnable();
+        Thread thread = new Thread(bombRunnable);
+        thread.start();
+        System.out.println("lab2_17_2 - method main finish");
+
+    }
+}
